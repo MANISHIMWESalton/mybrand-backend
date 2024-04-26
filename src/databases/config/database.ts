02fbfba1ec7c58f3e66de14c4e0cc940520dcd58
MANISHIMWESalton <manishimwejosephs@gmail.com>
2024-04-26 14:21:38 +0200

    remove bugs

--- src/databases/config/database.ts
+++ src/databases/config/database.ts
@@ -1,6 +1,6 @@
 import mongoose from 'mongoose';
 
-mongoose.connect("mongodb+srv://joseph:Urwanda28@cluster0.tbuska2.mongodb.net//mybrand").then(()=>{
+mongoose.connect("mongodb+srv://joseph:Urwanda28@cluster0.tbuska2.mongodb.net/mybrand").then(()=>{
     console.log("Connected")
 }).catch(err=>{
     console.log("Connectiong to database error: ", err)
